3baab8a272a882e3ebb42019276dcea60d7a4a36
Jagoda11 <jagoda.cubrilo@yahoo.com>
2025-07-29 20:58:49 +0200

    chore: add Prettier configuration and ignore files; clean up index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,5 @@
-// Define a simple function
 export function greet(name: string): void {
   // eslint-disable-next-line no-console
   console.log(`Hello, ${name}! 👋 Welcome to my project template.`)
 }
-// Call the function
 greet('User')
